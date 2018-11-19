91af10e98e5105e22770a11d75d70b93e3b9367b
luxiangya <wbsrz123>
2018-11-19 14:22:39 +0800

    添加注释

--- market-api/src/main/java/com/maxcar/statistics/controller/RankingController.java
+++ market-api/src/main/java/com/maxcar/statistics/controller/RankingController.java
@@ -2,7 +2,8 @@ package com.maxcar.statistics.controller;
 
 import com.maxcar.BaseController;
 import com.maxcar.base.pojo.InterfaceResult;
-import com.maxcar.statistics.model.request.GetInventoryRankingRequest;
+import com.maxcar.statistics.model.parameter.GetInventoryRankingParameter;
+import com.maxcar.statistics.model.request.GetInventoryRankingByConditionRequest;
 import com.maxcar.statistics.model.request.GetInvoiceRankingByConditionRequest;
 import com.maxcar.statistics.model.request.GetYesterdayInvoiceRankingRequest;
 import com.maxcar.statistics.service.RankingService;
@@ -10,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * describe: 排行统计相关
+ * create_date: lxy 2018/11/19  14:22
+ **/
 @RestController
 public class RankingController extends BaseController {
 
@@ -19,7 +24,7 @@ public class RankingController extends BaseController {
     @RequestMapping("/ranking/test")
     public InterfaceResult getPropertyContractAll() throws Exception {
 
-        GetInventoryRankingRequest reques =new GetInventoryRankingRequest();
+        GetInventoryRankingParameter reques =new GetInventoryRankingParameter();
 
        reques.setMarketId("007");
 
@@ -31,7 +36,15 @@ public class RankingController extends BaseController {
         request1.setEndTime("2018-11-15");
         request1.setMarketId("007");
         request1.setCarInvoiceType("1");
-        return getInterfaceResult("200", rankingService.getInvoiceRankingByCondition(request1));
+
+        GetInventoryRankingByConditionRequest request2 = new GetInventoryRankingByConditionRequest();
+
+        request2.setStartTime("2017-11-11");
+        request2.setEndTime("2018-11-15");
+        request2.setMarketId("007");
+        request2.setInventoryCycle(1);
+
+        return getInterfaceResult("200", rankingService.getInventoryRankingByCondition(request2));
     }
 
 }
