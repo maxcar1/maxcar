64e8d9603929775e77903361352e810dbabd2715
sunyazhou <15538111752@163.com>
2018-11-19 13:44:16 +0800

    syz

---
+++ market-stock/market-stock-interface/src/main/java/com/maxcar/stock/dao/CarReviewMapper.java
@@ -0,0 +1,30 @@
+package com.maxcar.stock.dao;
+
+import com.maxcar.stock.pojo.CarReview;
+import com.maxcar.stock.pojo.CarReviewExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CarReviewMapper {
+    int countByExample(CarReviewExample example);
+
+    int deleteByExample(CarReviewExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(CarReview record);
+
+    int insertSelective(CarReview record);
+
+    List<CarReview> selectByExample(CarReviewExample example);
+
+    CarReview selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") CarReview record, @Param("example") CarReviewExample example);
+
+    int updateByExample(@Param("record") CarReview record, @Param("example") CarReviewExample example);
+
+    int updateByPrimaryKeySelective(CarReview record);
+
+    int updateByPrimaryKey(CarReview record);
+}
---
+++ market-stock/market-stock-interface/src/main/java/com/maxcar/stock/pojo/CarReview.java
@@ -0,0 +1,85 @@
+package com.maxcar.stock.pojo;
+
+import java.util.Date;
+
+public class CarReview {
+    private Integer id;
+
+    private String carId;
+
+    private String outReason;
+
+    private Date backTime;
+
+    private Integer isPass;
+
+    private Integer isPledge;
+
+    private Date insertTime;
+
+    private Integer isValid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId == null ? null : carId.trim();
+    }
+
+    public String getOutReason() {
+        return outReason;
+    }
+
+    public void setOutReason(String outReason) {
+        this.outReason = outReason == null ? null : outReason.trim();
+    }
+
+    public Date getBackTime() {
+        return backTime;
+    }
+
+    public void setBackTime(Date backTime) {
+        this.backTime = backTime;
+    }
+
+    public Integer getIsPass() {
+        return isPass;
+    }
+
+    public void setIsPass(Integer isPass) {
+        this.isPass = isPass;
+    }
+
+    public Integer getIsPledge() {
+        return isPledge;
+    }
+
+    public void setIsPledge(Integer isPledge) {
+        this.isPledge = isPledge;
+    }
+
+    public Date getInsertTime() {
+        return insertTime;
+    }
+
+    public void setInsertTime(Date insertTime) {
+        this.insertTime = insertTime;
+    }
+
+    public Integer getIsValid() {
+        return isValid;
+    }
+
+    public void setIsValid(Integer isValid) {
+        this.isValid = isValid;
+    }
+}
