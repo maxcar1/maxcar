af2c317880fc809860980563a042397e82169470
hanling <1161466101@qq.com>
2018-11-20 18:44:45 +0800

    无锡新需求

--- market-stock/market-stock-interface/src/main/java/com/maxcar/stock/service/ReviewDetailService.java
+++ market-stock/market-stock-interface/src/main/java/com/maxcar/stock/service/ReviewDetailService.java
@@ -5,6 +5,6 @@ import com.maxcar.stock.pojo.ReviewDetail;
 import java.util.List;
 
 public interface ReviewDetailService {
-    List<ReviewDetail> getReviewDetail();
+    List<ReviewDetail> getReviewDetail(ReviewDetail reviewDetail);
 
 }
