c967ca222efcd9c456e13b6ef384dea5f653d20e
hanling <1161466101@qq.com>
2018-11-20 18:39:04 +0800

    无锡新需求

---
+++ market-stock/market-stock-service/src/main/java/com/maxcar/stock/service/impl/ReviewDetailServiceImpl.java
@@ -0,0 +1,15 @@
+package com.maxcar.stock.service.impl;
+
+import com.maxcar.stock.pojo.ReviewDetail;
+import com.maxcar.stock.service.ReviewDetailService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("reviewDetailService")
+public class ReviewDetailServiceImpl implements ReviewDetailService {
+    @Override
+    public List<ReviewDetail> getReviewDetail() {
+        return null;
+    }
+}
