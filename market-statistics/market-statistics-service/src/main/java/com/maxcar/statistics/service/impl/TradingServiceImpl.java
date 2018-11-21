cf2b2b7820d773ade8111d2cf8ba450dd64a65e5
shenzhongzong <shenzhongzong@126.com>
2018-11-21 10:09:57 +0800

    添加实现类中的方法

--- market-statistics/market-statistics-service/src/main/java/com/maxcar/statistics/service/impl/TradingServiceImpl.java
+++ market-statistics/market-statistics-service/src/main/java/com/maxcar/statistics/service/impl/TradingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.maxcar.statistics.service.impl;
 
 import com.maxcar.base.util.DateUtils;
+import com.maxcar.statistics.dao.TradingDao;
 import com.maxcar.statistics.model.request.TradingRequest;
 import com.maxcar.statistics.model.response.TradingResponse;
 import com.maxcar.statistics.service.TradingService;
@@ -8,6 +9,7 @@ import com.maxcar.statistics.service.impl.mapperService.TradingMapperService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
 import java.util.*;
 
 @Service("tradingService")
@@ -143,7 +145,7 @@ public class TradingServiceImpl implements TradingService {
             String yearAgoMonth = DateUtils.getYearAgoMonth(month);
             Date date = DateUtils.parseDate(yearAgoMonth, DateUtils.DATE_FORMAT_DATEONLY);
             Date monthEnd = DateUtils.getMonthEnd(date);
-            String s = DateUtils.formatDate(monthEnd, DateUtils.DATE_FORMAT_DATEONLY);
+            String s = DateUtils.formatDate(monthEnd, DateUtils.DATE_FORMAT_DATETIME);
             tradingRequest.setTimeStart(yearAgoMonth);
             tradingRequest.setTimeEnd(s);
             List<TradingResponse> oneYearlist = tradingMapperService.getVolumeAndValue(tradingRequest);
@@ -162,7 +164,7 @@ public class TradingServiceImpl implements TradingService {
             String monthUp = DateUtils.getAgoMonth(month);
             Date dateUp = DateUtils.parseDate(monthUp, DateUtils.DATE_FORMAT_DATEONLY);
             Date monthUpEnd = DateUtils.getMonthEnd(dateUp);
-            String up = DateUtils.formatDate(monthUpEnd, DateUtils.DATE_FORMAT_DATEONLY);
+            String up = DateUtils.formatDate(monthUpEnd, DateUtils.DATE_FORMAT_DATETIME);
             tradingRequest.setTimeStart(monthUp);
             tradingRequest.setTimeEnd(up);
             List<TradingResponse> oneMonthList = tradingMapperService.getVolumeAndValue(tradingRequest);
@@ -178,5 +180,91 @@ public class TradingServiceImpl implements TradingService {
             response.setAvgPriceMonth(avgUp);
         }
         return list;
+    }
+
+    @Override
+    public Map<String, Double> getTenantCount(TradingRequest tradingRequest) {
+        tradingRequest.setCarNumMin(0);
+        tradingRequest.setCarNumMax(10);
+        Double count = tradingMapperService.countCarNum(tradingRequest);
+        Map<String,Double> map = new LinkedHashMap();
+        map.put("0",count);
+
+        tradingRequest.setCarNumMin(10);
+        tradingRequest.setCarNumMax(20);
+        count = tradingMapperService.countCarNum(tradingRequest);
+        map.put("10",count);
+
+        tradingRequest.setCarNumMin(20);
+        tradingRequest.setCarNumMax(30);
+        count = tradingMapperService.countCarNum(tradingRequest);
+        map.put("20",count);
+
+        tradingRequest.setCarNumMin(30);
+        tradingRequest.setCarNumMax(40);
+        count = tradingMapperService.countCarNum(tradingRequest);
+        map.put("30",count);
+
+        tradingRequest.setCarNumMin(40);
+        tradingRequest.setCarNumMax(50);
+        count = tradingMapperService.countCarNum(tradingRequest);
+        map.put("40",count);
+
+        tradingRequest.setCarNumMin(50);
+        tradingRequest.setCarNumMax(0);
+        count = tradingMapperService.countCarNum(tradingRequest);
+        map.put("50",count);
+        return map;
+
+    }
+
+    @Override
+    public List<TradingResponse> getTenantDeal(TradingRequest tradingRequest) throws Exception {
+        List<TradingResponse> list = tradingMapperService.getTenantMonthNum(tradingRequest);
+        for(TradingResponse tr : list){
+            String month = tr.getMonth();
+            month += "-01";
+            //  同比
+            Double tenantCount = tr.getTenantCount();
+            Double tenantCount1 = 0.0;
+            String yearAgoMonth = DateUtils.getYearAgoMonth(month);
+            Date date = DateUtils.parseDate(yearAgoMonth, DateUtils.DATE_FORMAT_DATEONLY);
+            Date monthEnd = DateUtils.getMonthEnd(date);
+            String s = DateUtils.formatDate(monthEnd,DateUtils.DATE_FORMAT_DATETIME);
+            tradingRequest.setTimeStart(yearAgoMonth);
+            tradingRequest.setTimeEnd(s);
+            List<TradingResponse> oneYearlist = tradingMapperService.getTenantMonthNum(tradingRequest);
+            if(oneYearlist.size() > 0){
+                TradingResponse response = oneYearlist.get(0);
+                tenantCount1 = response.getTenantCount();
+            }
+            Double yearRate = (tenantCount - tenantCount1) / (tenantCount1 == 0 ? 1 : tenantCount1);
+            yearRate = (double) Math.round((yearRate * 100)) / 100;
+            tr.setAvgPriceYear(yearRate);
+
+            //  环比
+            String monthAgoMonth = DateUtils.getYearAgoMonth(month);
+            Date date1 = DateUtils.parseDate(monthAgoMonth, DateUtils.DATE_FORMAT_DATEONLY);
+            Date monthEnd1 = DateUtils.getMonthEnd(date1);
+            String s1 = DateUtils.formatDate(monthEnd1,DateUtils.DATE_FORMAT_DATETIME);
+            tradingRequest.setTimeStart(monthAgoMonth);
+            tradingRequest.setTimeEnd(s1);
+            List<TradingResponse> oneMonthlist = tradingMapperService.getTenantMonthNum(tradingRequest);
+            tenantCount1 = 0.0;
+            if(oneYearlist.size() > 0){
+                TradingResponse response = oneYearlist.get(0);
+                tenantCount1 = response.getTenantCount();
+            }
+            Double MonthRate = (tenantCount - tenantCount1) / (tenantCount1 == 0 ? 1 : tenantCount1);
+            MonthRate = (double) Math.round((MonthRate * 100)) / 100;
+            tr.setAvgPriceMonth(MonthRate);
+        }
+
+        return list;
+    }
+
+    @Override
+    public void getCarPrice(TradingRequest tradingRequest) {
+
     }
 }
