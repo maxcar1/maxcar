28de4a3e8fdc1b4f4e7b3fb529088d19485ab627
hanling <1161466101@qq.com>
2018-11-21 16:21:21 +0800

    序列化

--- market-stock/market-stock-interface/src/main/java/com/maxcar/stock/pojo/CarReview.java
+++ market-stock/market-stock-interface/src/main/java/com/maxcar/stock/pojo/CarReview.java
@@ -1,8 +1,9 @@
 package com.maxcar.stock.pojo;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class CarReview {
+public class CarReview implements Serializable {
     private Integer id;
 
     private String carId;
