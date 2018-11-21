a784455bc7ec77611b620eb45907d5c58bc1783d
hanling <1161466101@qq.com>
2018-11-21 09:55:30 +0800

    无锡新需求

---
+++ market-stock/market-stock-interface/src/main/java/com/maxcar/stock/service/ReviewStepService.java
@@ -0,0 +1,9 @@
+package com.maxcar.stock.service;
+
+import com.maxcar.stock.pojo.ReviewStep;
+
+import java.util.List;
+
+public interface ReviewStepService {
+    List<ReviewStep> getReviewStep(ReviewStep reviewStep);
+}
