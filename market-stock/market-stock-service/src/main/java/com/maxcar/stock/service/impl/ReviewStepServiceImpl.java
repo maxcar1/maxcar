2a234cd1391b0f45fb0dba46888b41d3d8e3175e
hanling <1161466101@qq.com>
2018-11-21 09:56:47 +0800

    无锡新需求

---
+++ market-stock/market-stock-service/src/main/java/com/maxcar/stock/service/impl/ReviewStepServiceImpl.java
@@ -0,0 +1,13 @@
+package com.maxcar.stock.service.impl;
+
+import com.maxcar.stock.pojo.ReviewStep;
+import com.maxcar.stock.service.ReviewStepService;
+
+import java.util.List;
+
+public class ReviewStepServiceImpl implements ReviewStepService {
+    @Override
+    public List<ReviewStep> getReviewStep(ReviewStep reviewStep) {
+        return null;
+    }
+}
