0d5ca10d51d1fa06039a6187bd9487ef5eff6a5c
shenzhongzong <shenzhongzong@126.com>
2018-11-21 10:12:24 +0800

    sql 语句封装

--- market-statistics/market-statistics-service/src/main/java/com/maxcar/statistics/dao/TradingDao.java
+++ market-statistics/market-statistics-service/src/main/java/com/maxcar/statistics/dao/TradingDao.java
@@ -8,9 +8,15 @@ import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public interface TradingDao {
     @SelectProvider(type = TradingProvider.class, method = "getVolumeAndValue")
     List<TradingResponse> getVolumeAndValue(TradingRequest tradingRequest);
 
+    @SelectProvider(type = TradingProvider.class, method = "countCarNum")
+    Double countCarNum(TradingRequest tradingRequest);
+
+    @SelectProvider(type = TradingProvider.class, method = "getTenantMonthNum")
+    List<TradingResponse> getTenantMonthNum(TradingRequest tradingRequest);
 }
