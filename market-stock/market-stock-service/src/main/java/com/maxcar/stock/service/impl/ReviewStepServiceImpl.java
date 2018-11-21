ef9a971adfe587fca74106a045e252b667a2c892
hanling <1161466101@qq.com>
2018-11-21 10:04:10 +0800

    无锡新需求

--- market-stock/market-stock-service/src/main/java/com/maxcar/stock/service/impl/ReviewStepServiceImpl.java
+++ market-stock/market-stock-service/src/main/java/com/maxcar/stock/service/impl/ReviewStepServiceImpl.java
@@ -1,13 +1,24 @@
 package com.maxcar.stock.service.impl;
 
+import com.maxcar.stock.mapper.ReviewStepMapper;
 import com.maxcar.stock.pojo.ReviewStep;
+import com.maxcar.stock.pojo.ReviewStepExample;
 import com.maxcar.stock.service.ReviewStepService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("reviewStepService")
 public class ReviewStepServiceImpl implements ReviewStepService {
+    @Autowired
+    private ReviewStepMapper reviewStepMapper;
+
     @Override
     public List<ReviewStep> getReviewStep(ReviewStep reviewStep) {
-        return null;
+        ReviewStepExample example = new ReviewStepExample();
+        example.createCriteria().andApplyTypeEqualTo(1).andMarketIdEqualTo(reviewStep.getMarketId());
+        List<ReviewStep> list = reviewStepMapper.selectByExample(example);
+        return list;
     }
 }
