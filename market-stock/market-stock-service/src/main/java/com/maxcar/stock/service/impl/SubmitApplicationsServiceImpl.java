c705fbfd512f8ff5f75e48a5e1d8bf615ca1143a
luoqin <1636759311@qq.com>
2018-11-23 11:32:10 +0800

    DD

---
+++ market-stock/market-stock-service/src/main/java/com/maxcar/stock/service/impl/SubmitApplicationsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.maxcar.stock.service.impl;
+
+import com.maxcar.base.pojo.InterfaceResult;
+import com.maxcar.stock.dao.CarReviewMapper;
+import com.maxcar.stock.pojo.CarReview;
+import com.maxcar.stock.service.SubmitApplicationsService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SubmitApplicationsServiceImpl implements SubmitApplicationsService {
+    @Autowired
+    private CarReviewMapper carReviewMapper;
+
+
+    @Override
+    public InterfaceResult insertRecord(CarReview record) {
+
+        InterfaceResult interfaceResult = new InterfaceResult();
+		 int count = 0;
+	     count = carReviewMapper.insertSelective(record);
+	     if(count!=0){
+	    	 interfaceResult.InterfaceResult200("新增成功");
+
+	     }else{
+	    	 interfaceResult.InterfaceResult600("新增失败");
+	     }
+
+	        return interfaceResult;
+	}
+
+}
