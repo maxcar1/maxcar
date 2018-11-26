79d125a0e2a70f26602c77ce0755ffc28eb9d0fa
yangshujun <yangshujun>
2018-11-26 15:50:33 +0800

    多线程建立连接

--- barrier/src/main/java/com/maxcar/mqtt/service/ServerMQTT.java
+++ barrier/src/main/java/com/maxcar/mqtt/service/ServerMQTT.java
@@ -10,6 +10,8 @@ import javax.annotation.PostConstruct;
 import java.util.Calendar;
 import java.util.Random;
 import java.util.Date;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Title:Server 这是发送消息的服务端
@@ -17,16 +19,33 @@ import java.util.Date;
  * @author luoshunfeng
  */
 
-public class ServerMQTT {
+public class ServerMQTT extends Thread{
 
     static Logger logger = LoggerFactory.getLogger(ServerMQTT.class);
     private MqttClient client;
     private String mqttClientHost;
     private String clientId;
-    private MqttTopic mqttTopic;
+ //   private MqttTopic mqttTopic;
     private MqttMessage message;
     private String username;
     private String password;
+
+    private byte[] data;
+    private String topic;
+
+    private static Lock lock = new ReentrantLock();
+
+    public ServerMQTT(String topic,byte[] data)throws MqttException{
+        this.topic = topic;
+        this.data = data;
+        long timestamps = System.currentTimeMillis();
+        clientId = String.valueOf(timestamps)+UuidUtils.getRandByNum(4);
+        mqttClientHost = LoadProperties.getProperties_3("../../../application.properties","mqtt.client.host");
+        username = LoadProperties.getProperties_3("../../../application.properties","mqtt.server.username");
+        password = LoadProperties.getProperties_3("../../../application.properties","mqtt.server.password");
+        //host为主机名,test为clientid即连接MQTT的客户端ID,一般以客户端唯一标识符表示,MemoryPersistence设置clientid的保存形式,默认为以内存保存
+        client = new MqttClient(mqttClientHost, clientId, new MemoryPersistence());
+    }
 
     /**
      * 构造函数
@@ -34,7 +53,8 @@ public class ServerMQTT {
      */
     public ServerMQTT()throws MqttException{
         /*clientId = LoadProperties.getProperties_3("../../../application.properties","serviceid");*/
-        clientId = UuidUtils.getRandByNum(6);
+        long timestamps = System.currentTimeMillis();
+        clientId = String.valueOf(timestamps)+UuidUtils.getRandByNum(4);
         mqttClientHost = LoadProperties.getProperties_3("../../../application.properties","mqtt.client.host");
         username = LoadProperties.getProperties_3("../../../application.properties","mqtt.server.username");
         password = LoadProperties.getProperties_3("../../../application.properties","mqtt.server.password");
@@ -52,18 +72,22 @@ public class ServerMQTT {
      */
     private void connect(String topic) {
         MqttConnectOptions options = new MqttConnectOptions();
-        options.setCleanSession(true);
+        options.setCleanSession(false);
         // 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录,这里设置为true表示每次连接到服务器都以新的身份连接
         options.setUserName(username);
         options.setPassword(password.toCharArray());
         // 设置超时时间
-        options.setConnectionTimeout(10);
+        options.setConnectionTimeout(60*10);
         // 设置会话心跳时间 单位为秒 服务器会每隔1.5*10秒的时间向客户端发送个消息判断客户端是否在线,但这个方法并没有重连的机制
-        options.setKeepAliveInterval(10);
+        options.setKeepAliveInterval(60*10);
         try {
             client.setCallback(new PushCallback());
             client.connect(options);
-            mqttTopic = client.getTopic(topic);
+           /* mqttTopic = client.getTopic(topic);
+            if (null == mqttTopic){
+                ServerMQTT serverMQTT = new ServerMQTT();
+                mqttTopic = serverMQTT.client.getTopic(topic);
+            }*/
             logger.info("已建立客户端连接,准备发送消息");
         } catch (Exception e) {
             e.printStackTrace();
@@ -78,15 +102,23 @@ public class ServerMQTT {
      */
     public void publish(MqttMessage message,String topic) throws MqttPersistenceException,
             MqttException {
-        logger.info("发送消息主体! "+mqttTopic+"内容"+message);
-        if (null == mqttTopic){
-            ServerMQTT serverMQTT = new ServerMQTT();
-            serverMQTT.init(topic);
+        try {
+            lock.lock();
+            logger.info("发送消息主体! "+topic+"内容"+message);
+            if (null == client){
+                ServerMQTT se = new ServerMQTT();
+                this.client = se.client;
+            }
+            client.publish(topic,message);
+/*
+            MqttDeliveryToken token = mqttTopic.publish(message);
+            token.waitForCompletion();*/
+            /*logger.info("服务端消息已经发送! "
+                    + token.isComplete());*/
+            logger.info("服务端消息已经发送!");
+        }finally {
+            lock.unlock();
         }
-        MqttDeliveryToken token = mqttTopic.publish(message);
-        token.waitForCompletion();
-        logger.info("服务端消息已经发送! "
-                + token.isComplete());
     }
 
     /**
@@ -146,6 +178,7 @@ public class ServerMQTT {
 
     public void send(byte[] data,String topic) {
         try {
+            lock.lock();
 //            String TOPIC = (topic == null || topic.equals("")) ? TOPIC : topic;
 //            topic11 = client.getTopic((topic == null || topic.equals("")) ? TOPIC : topic);
             this.init(topic);
@@ -157,8 +190,18 @@ public class ServerMQTT {
             logger.info(this.message.isRetained() + "------ratained状态");
         }catch (Exception ex){
             ex.printStackTrace();
+        }finally {
+            /*try {
+                client.close();
+            }catch (MqttException e){
+                e.printStackTrace();
+            }*/
+            lock.unlock();
         }
     }
 
-
+    @Override
+    public synchronized void run() {
+        this.send(data,topic);
+    }
 }
