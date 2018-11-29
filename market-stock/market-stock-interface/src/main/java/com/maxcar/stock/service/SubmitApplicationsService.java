b2f2acbf2da69d08de5a33c2c939907300075f3e
luoqin <1636759311@qq.com>
2018-11-29 15:10:04 +0800

    fix: bug

--- market-stock/market-stock-interface/src/main/java/com/maxcar/stock/service/SubmitApplicationsService.java
+++
@@ -1,8 +0,0 @@
-package com.maxcar.stock.service;
-
-import com.maxcar.base.pojo.InterfaceResult;
-import com.maxcar.stock.pojo.CarReview;
-
-public interface SubmitApplicationsService {
-    public InterfaceResult insertRecord(CarReview carReview);
-}
