0de10aa42804e769b01a60611773e5323fcf29f8
shunfengluo <luoshunfeng>
2018-12-05 13:57:12 +0800

    出场审批记录同步本地

---
+++ barrier/src/main/java/com/maxcar/stock/dao/CarReviewMapper.java
@@ -0,0 +1,31 @@
+package com.maxcar.stock.dao;
+
+import com.maxcar.stock.pojo.CarReview;
+import com.maxcar.stock.pojo.CarReviewExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface CarReviewMapper {
+    int countByExample(CarReviewExample example);
+
+    int deleteByExample(CarReviewExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(CarReview record);
+
+    int insertSelective(CarReview record);
+
+    List<CarReview> selectByExample(CarReviewExample example);
+
+    CarReview selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") CarReview record, @Param("example") CarReviewExample example);
+
+    int updateByExample(@Param("record") CarReview record, @Param("example") CarReviewExample example);
+
+    int updateByPrimaryKeySelective(CarReview record);
+
+    int updateByPrimaryKey(CarReview record);
+}
