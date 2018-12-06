acb3038c4ca842ee86964c8aefef2979c06bbb73
shenzhongzong <shenzhongzong@126.com>
2018-12-06 16:38:30 +0800

    库存统计和总览   排名除外

--- market-statistics/market-statistics-service/src/main/java/com/maxcar/statistics/dao/CarpriceDayMapper.java
+++ market-statistics/market-statistics-service/src/main/java/com/maxcar/statistics/dao/CarpriceDayMapper.java
@@ -1,13 +1,10 @@
 package com.maxcar.statistics.dao;
 
-import com.maxcar.market.pojo.Invoice;
 import com.maxcar.statistics.model.entity.CarpriceDayEntity;
 import com.maxcar.statistics.model.request.TradingRequest;
 import com.maxcar.statistics.model.response.TradingResponse;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Map;
 
 public interface CarpriceDayMapper {
     int deleteByPrimaryKey(Integer id);
@@ -26,5 +23,5 @@ public interface CarpriceDayMapper {
 
     List<TradingResponse> stockAvgDay(TradingRequest tradingRequest);
 
-    List<Invoice> InsertCarpriceDay();
+    void InsertCarpriceDay();
 }
