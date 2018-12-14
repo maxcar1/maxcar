fe65efa51ae9a98e9ba0e96dbfb7dfae40c6c848
shenzhongzong <shenzhongzong@126.com>
2018-12-14 17:34:54 +0800

    开票管理  数据导出缩短到3个月

--- market-api/src/main/java/com/maxcar/market/controller/InvoiceController.java
+++ market-api/src/main/java/com/maxcar/market/controller/InvoiceController.java
@@ -584,112 +584,78 @@ public class InvoiceController extends BaseController {
     public InterfaceResult detailsExcel(@RequestBody Invoice invoice, HttpServletRequest request) throws Exception {
         InterfaceResult interfaceResult = new InterfaceResult();
 
-        User currentUser = getCurrentUser(request);
-        String userId = currentUser.getUserId();
-        if (null != currentUser.getMarketId() && !"".equals(currentUser.getMarketId())) {
-            invoice.setMarketId(currentUser.getMarketId());
+        int monthGap = 0;
+        String billTimeStart = invoice.getBillTimeStart();
+        String billTimeEnd = invoice.getBillTimeEnd();
+        if (StringUtil.isNotEmpty(billTimeEnd)) {
+            String[] start = billTimeStart.split("-");
+            String[] end = billTimeEnd.split("-");
+            int startMonth = Integer.parseInt(start[1]);
+            int startYear = Integer.parseInt(start[0]);
+
+            int endMonth = Integer.parseInt(end[1]);
+            int endYaer = Integer.parseInt(end[0]);
+            if (startMonth != endMonth) {
+                while (true) {
+                    endMonth -= 1;
+                    if (endMonth < 1) {
+                        endMonth = 12;
+                        endYaer -= 1;
+                    }
+                    monthGap += 1;
+                    if (endMonth == startMonth && startYear == endYaer) {
+                        break;
+                    }
+                }
+            }
         }
-        User user = userService.selectByPrimaryKey(userId);
-        if (user.getManagerFlag() == 0) {
-            invoice.setMarketId(null);
-        } else if (user.getManagerFlag() == 2) {
-            invoice.setMarketId(currentUser.getMarketId());
+        if (monthGap > 3) {
+            interfaceResult.InterfaceResult600("数据量过大，影响导出速率，建议选择3个月以内数据");
         } else {
-            invoice.setUserId(currentUser.getUserId());
+            User currentUser = getCurrentUser(request);
+            String userId = currentUser.getUserId();
+            if (null != currentUser.getMarketId() && !"".equals(currentUser.getMarketId())) {
+                invoice.setMarketId(currentUser.getMarketId());
+            }
+            User user = userService.selectByPrimaryKey(userId);
+            if (user.getManagerFlag() == 0) {
+                invoice.setMarketId(null);
+            } else if (user.getManagerFlag() == 2) {
+                invoice.setMarketId(currentUser.getMarketId());
+            } else {
+                invoice.setUserId(currentUser.getUserId());
+            }
+
+            if (StringUtil.isNotEmpty(billTimeEnd)) {
+                Date date = DateUtils.parseDate(billTimeEnd, DateUtils.DATE_FORMAT_DATEONLY);
+                Date dayEnd = DateUtils.getDayEnd(date);
+                String s = DateUtils.formatDate(dayEnd);
+                invoice.setBillTimeStart(invoice.getBillTimeStart());
+                invoice.setBillTimeEnd(s);
+            } else {
+                Date date = new Date();
+                Date dayEnd = DateUtils.getDayEnd(date);
+                String s = DateUtils.formatDate(dayEnd);
+                String[] split = s.split("-");
+                Integer year = Integer.parseInt(split[0]);
+                Integer month = Integer.parseInt(split[1]);
+                String day = split[2];
+                for (int i = 0; i < 3; i++) {
+                    if (month > 1) {
+                        month = month - 1;
+                    } else {
+                        year = year - 1;
+                        month = 12;
+                    }
+                }
+                String startTime = year.toString() + "-" + month.toString() + "-" + day;
+                invoice.setBillTimeStart(startTime.substring(0, 10));
+                invoice.setBillTimeEnd(s);
+            }
+
+            List<InvoiceExcel> lists = invoiceService.detailsManage(invoice);
+            interfaceResult.InterfaceResult200(lists);
         }
-
-//        List<TradeInformation> tradeInformations = invoiceService.detailsExcel(invoice);
-        List<InvoiceExcel> lists = invoiceService.detailsManage(invoice);
-//        List<Map> list = new ArrayList<>();
-//        for (Invoice i : lists) {
-//            Map<String, Object> map = new LinkedHashMap<>();
-//            //  发票代码
-//            String invoiceCode = i.getInvoiceCode();
-//            if (StringUtil.isNotEmpty(invoiceCode)) {
-//                map.put("invoiceCode", invoiceCode);
-//            } else {
-//                map.put("invoiceCode", "");
-//            }
-//            //  发票号码
-//            String invoiceNo = i.getInvoiceNo();
-//            if (StringUtil.isNotEmpty(invoiceNo)) {
-//                map.put("invoiceNo", invoiceNo);
-//            } else {
-//                map.put("invoiceNo", "");
-//            }
-//            //  开票时间
-//            Date billTime = i.getBillTime();
-//            if (billTime != null) {
-//                map.put("billTime", billTime);
-//            } else {
-//                map.put("billTime", "");
-//            }
-//            //  买方
-//            String purchacerName = i.getPurchacerName();
-//            if (StringUtil.isNotEmpty(purchacerName)) {
-//                map.put("purchacerName", purchacerName);
-//            } else {
-//                map.put("purchacerName", "");
-//            }
-//            //  卖方
-//            String sellerName = i.getSellerName();
-//            if (StringUtil.isNotEmpty(sellerName)) {
-//                map.put("sellerName", sellerName);
-//            } else {
-//                map.put("sellerName", "");
-//            }
-//            //  车价合计
-//            Double price = i.getPrice();
-//            if (price != 0 && price != null) {
-//                map.put("price", price);
-//            } else {
-//                map.put("price", "");
-//            }
-//            //  开票端口
-//            Integer invoicePortof = i.getInvoicePortof();
-//            if (invoicePortof != null) {
-//                if (invoicePortof == 0) {
-//                    map.put("invoicePortof", "自助交易终端");
-//                } else if(invoicePortof == 1) {
-//                    map.put("invoicePortof", "商户APP");
-//                }else if(invoicePortof == 2) {
-//                    map.put("invoicePortof", "窗口");
-//                }
-//            } else {
-//                map.put("invoicePortof", "");
-//            }
-//            //  开票端口
-//            Integer carSources = i.getCarSources();
-//            if (carSources != 0 && carSources != null) {
-//                if (carSources == 1) {
-//                    map.put("carSources", "商品车");
-//                } else if(carSources == 2) {
-//                    map.put("carSources", "挂靠");
-//                }else if(carSources == 3) {
-//                    map.put("carSources", "代办");
-//                }else if(carSources == 4) {
-//                    map.put("carSources", "散户");
-//                }
-//            } else {
-//                map.put("carSources", "");
-//            }
-//            //  开票端口
-//            Integer invoiceStatus = i.getInvoiceStatus();
-//            if (invoiceStatus != null) {
-//                if (invoiceStatus == 1) {
-//                    map.put("invoiceStatus", "未处理");
-//                } else if(invoiceStatus == 0) {
-//                    map.put("invoiceStatus", "作废");
-//                }else if(invoiceStatus == 2) {
-//                    map.put("invoiceStatus", "已处理");
-//                }
-//            } else {
-//                map.put("invoiceStatus", "");
-//            }
-//            list.add(map);
-//        }
-
-        interfaceResult.InterfaceResult200(lists);
 
         return interfaceResult;
     }
